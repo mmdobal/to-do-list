c8695597c2d4f089773c0397d82cea2efd93774c
Marianna <mddobal@gmail.com>
2019-07-22 20:11:35 -0300

    Updates

---
+++ precache-manifest.36bf54784ffd6f2b30100094f8cd072b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea529faeff1295f4c552e8f96f97218",
+    "url": "/xp-todo/index.html"
+  },
+  {
+    "revision": "c426ae3d7f9010130198",
+    "url": "/xp-todo/static/css/main.bc1fa9e0.chunk.css"
+  },
+  {
+    "revision": "3b275e7459e809705f1a",
+    "url": "/xp-todo/static/js/2.0128ec8a.chunk.js"
+  },
+  {
+    "revision": "c426ae3d7f9010130198",
+    "url": "/xp-todo/static/js/main.bd91c87a.chunk.js"
+  },
+  {
+    "revision": "768b0627cff92bb83b8f",
+    "url": "/xp-todo/static/js/runtime~main.3b73d3b3.js"
+  }
+]);
